b64d4d7c5d9232e23fd861ef09b1167cfc2c9042
Jaylen Smith <jaylensmith4521@gmail.com>
2023-03-08 14:30:10 -0500

    New Commit

--- Diamonds/ViewController.m
+++ Diamonds/ViewController.m
@@ -9,13 +9,31 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
 
+NSString *imagesStr[3] = {@"diamond", @"Icon-29", @"Icon-40"};
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.navigationItem.title = @"Diamonds";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"diamond"];
+    cell.textLabel.text = imagesStr[indexPath.item];
+    cell.imageView.image = [UIImage imageNamed: imagesStr[indexPath.item]];
+        
+    return cell;
 }
 
 
